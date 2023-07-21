e891ff21d8cf33fb11d2e8b7cee1be829a15fa49
K. Gofron <kgofron@comcast.net>
2023-07-21 16:08:57 -0400

    Fix typo

--- iocs/motorSimIOC/iocBoot/iocMotorSim/st.cmd
+++ iocs/motorSimIOC/iocBoot/iocMotorSim/st.cmd
@@ -1,3 +1,4 @@
+#!../../bin/linux-x86_64/motorSim
 # The is the ASYN example for communication to 4 simulated motors
 # "#!" marks lines that can be uncommented.
 
@@ -8,7 +9,7 @@ motorSim_registerRecordDeviceDriver(pdbbase)
 
 dbLoadTemplate("motor.substitutions")
 
-motorSimCreateController("motorSim1", 8)
+motorSimCreateController("motorSim1", 16)
 #asynSetTraceIOMask("motorSim1", 0, HEX)
 #asynSetTraceMask("motorSim1", 0, ERROR|FLOW|DRIVER)
 
@@ -21,6 +22,15 @@ motorSimConfigAxis("motorSim1", 4, 20000, -20000,  500, 0)
 motorSimConfigAxis("motorSim1", 5, 20000, -20000, 1500, 0)
 motorSimConfigAxis("motorSim1", 6, 20000, -20000, 2500, 0)
 motorSimConfigAxis("motorSim1", 7, 20000, -20000, 3000, 0)
+
+motorSimConfigAxis("motorSim1", 8, 20000, -20000,  500, 0)
+motorSimConfigAxis("motorSim1", 9, 20000, -20000, 1500, 0)
+motorSimConfigAxis("motorSim1", 10, 20000, -20000, 2500, 0)
+motorSimConfigAxis("motorSim1", 11, 20000, -20000, 3000, 0)
+motorSimConfigAxis("motorSim1", 12, 20000, -20000,  500, 0)
+motorSimConfigAxis("motorSim1", 13, 20000, -20000, 1500, 0)
+motorSimConfigAxis("motorSim1", 14, 20000, -20000, 2500, 0)
+motorSimConfigAxis("motorSim1", 15, 20000, -20000, 3000, 0)
 
 iocInit
 
